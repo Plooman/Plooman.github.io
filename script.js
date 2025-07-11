142e431e616b4a278b145e3b5cb31030a4c05519
Plooman <fahmanzaidan1@gmail.com>
2025-07-11 20:24:08 +0700

    memberikan animasi side bar berupa pergeseran dari kiri kekanan saat memencet menu dan disesuaikan main contentnya ketika side bar di tutup kekiri beserta memberikan icon untuk pemilihan menu di side barnya

--- script.js
+++ script.js
@@ -4,20 +4,25 @@ function showPage(pageName){
     document.querySelector('.about-me-content').classList.add('hidden');
 
     document.querySelector(`.${pageName}-content`).classList.remove('hidden');
-    console.log(`${pageName}`);
     
     if(pageName === 'dashboard'){
         document.querySelector(`.${pageName}`).classList.add('menu-side-bar-selected');
         document.querySelector('.work-pleace').classList.remove('menu-side-bar-selected');
         document.querySelector('.about-me').classList.remove('menu-side-bar-selected');
+
+        document.querySelector('.header-title').textContent = 'Dashboard'
     }else if(pageName === 'work-pleace'){
         document.querySelector('.dashboard').classList.remove('menu-side-bar-selected');
         document.querySelector(`.${pageName}`).classList.add('menu-side-bar-selected');
         document.querySelector('.about-me').classList.remove('menu-side-bar-selected');
+
+        document.querySelector('.header-title').textContent = 'Work Place'
     }else if(pageName === 'about-me'){
         document.querySelector('.dashboard').classList.remove('menu-side-bar-selected');
         document.querySelector('.work-pleace').classList.remove('menu-side-bar-selected');
         document.querySelector(`.${pageName}`).classList.add('menu-side-bar-selected');
+
+        document.querySelector('.header-title').textContent = 'About Me'
     }
 
     
@@ -25,3 +30,28 @@ function showPage(pageName){
 document.querySelector('.dashboard').classList.add('menu-side-bar-selected');
 document.querySelector('.work-pleace-content').classList.add('hidden');
 document.querySelector('.about-me-content').classList.add('hidden');
+
+const icon = document.querySelector('.icon-menu img');
+const sidebar = document.querySelector('.side-bar');
+document.querySelector('.main-content')
+
+let open = false;
+
+icon.addEventListener('click', function () {
+console.log(`${open}`);
+    icon.classList.remove(open ? 'rotate-animation-kekanan' : 'rotate-animation-kekiri');
+    void icon.offsetWidth;
+    icon.classList.add(open ? 'rotate-animation-kekiri' : 'rotate-animation-kekanan');
+    if(open === false){
+        document.querySelector('.main-content').style.marginLeft = '';
+    }
+    else if(open === true){
+        document.querySelector('.main-content').style.marginLeft = '150px';
+    }
+
+    sidebar.classList.remove(open ? 'nutup-SideBar-animation' : 'buka-SideBar-animation');
+    void sidebar.offsetWidth;
+    sidebar.classList.add(open ? 'buka-SideBar-animation' : 'nutup-SideBar-animation');
+
+    open = !open;
+});
